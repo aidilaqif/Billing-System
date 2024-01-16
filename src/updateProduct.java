e406933c32fcfd4743d63b71eca611ddccff3ae9
AIDIL 'AQIF <aidil.aqif@gmail.com>
2024-01-16 15:09:00 +0800

    212901/002/CODE ERROR FIXING
    The suggested change involves adding a default case or ensuring that there is a switch branch for each possible value of the variable being switched on in the switch statement at line 79 in the 'updateProduct.java' file.

--- src/updateProduct.java
+++ src/updateProduct.java
@@ -87,7 +87,9 @@ public class updateProduct extends JPanel {
 							case "N/S & Electric":
 								company.setSelectedIndex(2);
 								break;
-							
+							default:
+								company.setSelectedIndex(0);
+								break;
 						}
 					}
 			}
